c7978248ec5f375db296abc5bdacf7d5a64a5c50
adam <adam@config.com>
2019-02-01 12:43:18 -0600

    fix picsend

--- picsend.py
+++ picsend.py
@@ -1,4 +1,4 @@
-
+import os
 import uuid
 import subprocess
 import time
@@ -15,8 +15,13 @@ def send_pic_to_server(filename, server):
 	data = subprocess.Popen(["curl", "-X", "POST", "-F", "file=@/home/pi/garage/"+filename, server], stdout=subprocess.PIPE).communicate()[0]
 	#sudo curl -X POST -F file=@/home/pi/Superman/image.jpg http://localhost:8000
 
+def delete_pic_on_disk(filename):
+	os.remove(filename)
+	
 server_id = "http://192.168.1.35:8040"
 picture_id = take_picture()
 print picture_id
 time.sleep(2)
 send_pic_to_server(picture_id, server_id)
+time.sleep(3)
+delete_pic_on_disk(picture_id)
